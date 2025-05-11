bafec6f519a4554ccc00f5c4a1d94dd296b66d42
Saul <123548756+DoVah1201@users.noreply.github.com>
2025-05-11 17:58:47 +0100

    actualizaciones varias

--- libros_autores/src/main/java/com/ejercicio2/libros_autores/service/AutorService.java
+++ libros_autores/src/main/java/com/ejercicio2/libros_autores/service/AutorService.java
@@ -3,7 +3,7 @@ package com.ejercicio2.libros_autores.service;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.stereotype.Service;
+
 
 import com.ejercicio2.libros_autores.model.Autor;
 import com.ejercicio2.libros_autores.repository.AutorRepository;
