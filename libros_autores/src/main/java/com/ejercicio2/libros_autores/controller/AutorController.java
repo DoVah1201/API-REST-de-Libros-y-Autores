262016374caecf1d44c067eef04529d277078f21
Saul <123548756+DoVah1201@users.noreply.github.com>
2025-05-11 18:23:28 +0100

    correccion error service

--- libros_autores/src/main/java/com/ejercicio2/libros_autores/controller/AutorController.java
+++ libros_autores/src/main/java/com/ejercicio2/libros_autores/controller/AutorController.java
@@ -3,7 +3,14 @@ package com.ejercicio2.libros_autores.controller;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.web.bind.annotation.*;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 import com.ejercicio2.libros_autores.model.Autor;
 import com.ejercicio2.libros_autores.service.AutorService;
--- libros_autores/src/main/java/com/ejercicio2/libros_autores/service/AutorService.java
+++ libros_autores/src/main/java/com/ejercicio2/libros_autores/service/AutorService.java
@@ -3,12 +3,13 @@ package com.ejercicio2.libros_autores.service;
 import java.util.List;
 import java.util.Optional;
 
-
+import org.springframework.stereotype.Service;
 
 
 import com.ejercicio2.libros_autores.model.Autor;
 import com.ejercicio2.libros_autores.repository.AutorRepository;
 
+@Service
 public class AutorService {
 
     private final AutorRepository autorRepository;
