31852b87c4efe1560a3e283a914f84a28b47ddb6
Saul <123548756+DoVah1201@users.noreply.github.com>
2025-05-11 17:32:46 +0100

    cambios varios

--- libros_autores/src/main/java/com/ejercicio2/libros_autores/model/Autor.java
+++ libros_autores/src/main/java/com/ejercicio2/libros_autores/model/Autor.java
@@ -1,5 +1,27 @@
 package com.ejercicio2.libros_autores.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+
+@Entity
+@Table(name = "Autor")
 public class Autor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+    private String nacionalidad;
+
+    @OneToMany
+    private List<Libro> libro;
+
+
 
 }
